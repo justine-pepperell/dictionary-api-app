42c48ac059d020ad86240259112e4885392b26b6
justine-pepperell <justine.pepperell>
2022-11-10 14:11:52 +1300

    files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState } from 'react'
+import { getPhonetics } from './api'
+
+
+function App() {
+
+  const [word, setWord] = useState('');
+  const [phoneticSpelling, setPhoneticsSpelling] = useState('')
+
+
+  return <main>
+    <h2>{phoneticSpelling}</h2>
+     
+      <form onSubmit={(e) => {
+        getPhonetics(word).then((res) => {
+          setPhoneticsSpelling(res);
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+        e.preventDefault();
+      }}>
+        <input type="text" name="word" value={word} onChange={(e) => {
+          //console.log(e.target.value)
+          setWord(e.target.value); //this updates the state value for 'word' declared in useState(''), also re-renders
+        }} placeholder="Enter a word" />
+        <input type="submit" value="Submit" />
+      </form>
+    </main>
+
+}
+
+export default App
+
